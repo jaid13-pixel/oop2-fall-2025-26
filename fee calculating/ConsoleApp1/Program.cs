ad0deee1e3d626b94a5c10141af8c14bbf64db73
jaid13-pixel <ibnashabit@gmail.com>
2025-12-09 21:57:25 +0600

    Add files via upload

---
+++ fee calculating/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+abstract class Member
+{
+    public string name;
+    public int memberId;
+
+    public abstract int CalculateFee();
+}
+
+class StudentMember : Member
+{
+    public override int CalculateFee()
+    {
+        return 300;
+    }
+}
+
+class RegularMember : Member
+{
+    public override int CalculateFee()
+    {
+        return 500;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Member s = new StudentMember();
+        Member r = new RegularMember();
+
+        Console.WriteLine("Student fee: " + s.CalculateFee());
+        Console.WriteLine("Regular fee: " + r.CalculateFee());
+    }
+}
